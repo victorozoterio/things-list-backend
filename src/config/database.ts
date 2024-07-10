205f72149fd6f665c19346e3647845fd0ea2066e
Victor Ozoterio <victorozoterio@gmail.com>
2024-07-09 21:52:00 -0300

    feat: configuração para consumir um banco de dados em nuvem

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,11 +7,7 @@ export const databaseConfig: TypeOrmModuleAsyncOptions = {
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => ({
     type: 'postgres',
-    host: configService.get('DB_HOST'),
-    port: configService.get('DB_PORT'),
-    username: configService.get('DB_USERNAME'),
-    password: configService.get('DB_PASSWORD'),
-    database: configService.get('DB_NAME'),
+    url: configService.get('DB_URI'),
     entities: [path.join(__dirname, '/../modules/**/**/*.entity{.ts,.js}')],
     migrations: [path.join(__dirname, '/../migrations/*{.ts,.js}')],
     migrationsRun: true,
