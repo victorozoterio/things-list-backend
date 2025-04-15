4b4e489f6adf9c9dc077d1e210d7cef47810e120
Victor Ozoterio <victorozoterio@gmail.com>
2025-04-14 22:47:54 -0300

    chore: database

--- src/config/database.ts
+++ src/config/database.ts
@@ -7,7 +7,12 @@ export const databaseConfig: TypeOrmModuleAsyncOptions = {
   inject: [ConfigService],
   useFactory: (configService: ConfigService) => ({
     type: 'postgres',
-    url: configService.get('DB_URI'),
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
+    ssl: process.env.DB_SSL_MODE === 'true' ? { rejectUnauthorized: false } : false,
     entities: [path.join(__dirname, '/../modules/**/**/*.entity{.ts,.js}')],
     migrations: [path.join(__dirname, '/../migrations/*{.ts,.js}')],
     migrationsRun: true,
